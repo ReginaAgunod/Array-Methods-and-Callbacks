5a743e15c0c5b097578bdc850fd2b02d05f2dd7a
Regina Agunod <53545239+ReginaAgunod@users.noreply.github.com>
2020-05-07 15:42:41 -0700

    finsihed code

--- index.js
+++ index.js
@@ -167,29 +167,3 @@ function getAverageGoals(data) {
 console.log("Task 10:", getAverageGoals(getFinals(fifaData)));
 
 
-
-
-/// STRETCH 🥅 //
-
-/* STRETCH 1: Write a function called getGoals() that accepts a parameter `data` and returns the team with the most goals score per appearance (average goals for) in the World Cup finals */
-
-function getGoals(/* code here */) {
-
-    /* code here */
-
-};
-
-getGoals();
-
-
-/* STRETCH 2: Write a function called badDefense() that accepts a parameter `data` and calculates the team with the most goals scored against them per appearance (average goals against) in the World Cup finals */
-
-function badDefense(/* code here */) {
-
-    /* code here */
-
-};
-
-badDefense();
-
-/* If you still have time, use the space below to work on any stretch goals of your chosing as listed in the README file. */
